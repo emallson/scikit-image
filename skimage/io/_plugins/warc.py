7d7a1c5c73330ca93c8f00f9022b9e7c45ba4054
Johnathan Smith <emallson@archlinux.us>
2015-01-26 16:24:22 -0500

    Fixed warc reader. Works now.

--- skimage/io/_plugins/warc.py
+++ skimage/io/_plugins/warc.py
@@ -11,7 +11,7 @@ def imread(record, dtype=None):
     record : WARC Record
 
     """
-    im = _imread.imread_from_blob(record.payload)
+    im = _imread.imread_from_blob(record.payload.read())
     if dtype is not None:
         im = convert(im, dtype)
     return im
